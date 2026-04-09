811621efa5a2a357584c8405f483d2f8458b8451
muhammadfaizansarwarMLab <faizan.sarwar@maqsoodlabs.com>
2026-04-09 12:48:01 +0500

    integrate posthog successfully

---
+++ src/lib/posthog.js
@@ -0,0 +1,10 @@
+import posthog from "posthog-js";
+
+export function initPostHog() {
+  posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+    api_host: import.meta.env.VITE_POSTHOG_HOST,
+    person_profiles: "identified_only",
+  });
+}
+
+export { posthog };
