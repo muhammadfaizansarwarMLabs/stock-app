5e3a83202a406885335c89365b47062a82889be4
muhammadfaizansarwarMLab <faizan.sarwar@maqsoodlabs.com>
2026-04-09 14:30:55 +0500

    redepoly app

--- src/lib/posthog.js
+++ src/lib/posthog.js
@@ -1,9 +1,26 @@
+// import posthog from "posthog-js";
+
+// export function initPostHog() {
+//   posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+//     api_host: import.meta.env.VITE_POSTHOG_HOST,
+//     person_profiles: "identified_only",
+//   });
+// }
+
+// export { posthog };
+
+
+
 import posthog from "posthog-js";
 
 export function initPostHog() {
+  if (typeof window === "undefined") return; // ensures client-only
+
   posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
     api_host: import.meta.env.VITE_POSTHOG_HOST,
     person_profiles: "identified_only",
+    autocapture: true,       // automatically tracks pageviews, clicks, and Web Vitals
+    capture_pageview: true,  // tracks initial pageview
   });
 }
 
